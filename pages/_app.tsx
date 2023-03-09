da8d86df0823ba7716b05ba35461711294146370
Trentin Gillis <trentin.gillis@gmail.com>
2023-03-08 19:57:47 -0700

    Update to use styled-components rather than scss modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import '@/styles/global.scss';
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import styles from '@/styles/pages/Home.module.scss';
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  background-color: pink;
+`;
 
 export default function Home() {
   return (
-    <main className={styles.layout}>
+    <Wrapper>
       <h1>Prime Condies</h1>
-    </main>
+    </Wrapper>
   );
 }
