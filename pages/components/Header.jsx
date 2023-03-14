cd90a1ce6d1fedaf8e03287c00d57302a4570d7f
Philip Riegert <riegertphilip@gmail.com>
2023-03-14 15:59:10 +0100

    Initial work on header component

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -3,34 +3,46 @@ import styled from 'styled-components';
 
 const HeaderWrapper = styled.main`
  height: 130px;
-
-  display: grid;
-  grid-template-columns: ;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center ;
 `;
 const LogoWrapper = styled.main`
   display: flex;
   height: 36px;
   width: 152px
   align-items: left;
+  flex-grow: 1;
+	flex-shrink: 0;
+	flex-basis: 33.3333%;
+	flex-basis: 200px;
+	display: flex;
+
+
 `;
 const SearchWrapper = styled.main`
   display: flex;
   height: 46px;
-
+width: 100%;
 justify-content: center;
+flex-grow: 1;
+flex-shrink: 0;
+
+flex-basis: 200px;
+display: flex;
 `;
 
 function Header() {
-    return (
-        <HeaderWrapper>
-            <LogoWrapper>
-                <img src="/logo.svg" alt="" />
-            </LogoWrapper>
-            <SearchWrapper>
-                <input type="text" placeholder='Search' />
-            </SearchWrapper>
-        </HeaderWrapper>
-    )
+  return (
+    <HeaderWrapper>
+      <LogoWrapper>
+        <img src="/logo.svg" alt="" />
+      </LogoWrapper>
+      <SearchWrapper>
+        <input type="text" placeholder='Search' />
+      </SearchWrapper>
+    </HeaderWrapper>
+  )
 }
 
 export default Header
