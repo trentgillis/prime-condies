8eb2da3ba8ca5f3c6b452e1a8967e516eb77560d
Philip Riegert <riegertphilip@gmail.com>
2023-03-20 12:07:27 +0000

    mobile styling for navbar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,33 +1,30 @@
 import styled from 'styled-components';
 
 import { Logo } from '@/components';
+import { SearchBar } from '../SeachBar/SearchBar';
 
 const Wrapper = styled.nav`
   height: 130px;
   display: flex;
   flex-wrap: wrap;
-  align-items: center;
-`;
+  align-items: left;
+  flex-direction: column;
+  gap: 16px;
+  margin: 16px 16px 16px;
+ @media(min-width: 450px) {
+    flex-direction: row;
+    height: 80px;
+    flex-wrap: wrap;
+    align-items: left;
+  } 
+`
 
-const SearchWrapper = styled.main`
-  display: flex;
-  height: 46px;
-  width: 100%;
-  justify-content: center;
-  flex-grow: 1;
-  flex-shrink: 0;
-
-  flex-basis: 200px;
-  display: flex;
-`;
 
 export function Header() {
   return (
     <Wrapper>
       <Logo />
-      <SearchWrapper>
-        <input type="text" placeholder="Search" />
-      </SearchWrapper>
+      <SearchBar />
     </Wrapper>
   );
 }
