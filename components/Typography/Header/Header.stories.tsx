231547066798066709ca4fa116e1f551892c434d
Trentin Gillis <trentin.gillis@gmail.com>
2023-04-06 18:08:43 -0600

    Clean up Header story

--- components/Typography/Header/Header.stories.tsx
+++ components/Typography/Header/Header.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Header as HeaderComponent } from './Header';
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof HeaderComponent> = {
   title: 'Typography',
   component: HeaderComponent,
@@ -11,7 +10,6 @@ const meta: Meta<typeof HeaderComponent> = {
 export default meta;
 type Story = StoryObj<typeof HeaderComponent>;
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
 export const Header: Story = {
   args: {
     variant: 'H1',
