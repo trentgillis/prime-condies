73c649210a79d264b3534e224a4f7077ae73b662
Trentin Gillis <trentin.gillis@gmail.com>
2023-04-09 17:46:03 -0600

    Implement Snow icon component

--- components/WeatherIcon/icons/index.ts
+++ components/WeatherIcon/icons/index.ts
@@ -2,3 +2,4 @@ export * from './ChanceOfRainIcon/ChanceOfRainIcon';
 export * from './ChanceOfSnowIcon/ChanceOfSnowIcon';
 export * from './PartlyCloudyIcon/PartlyCloudyIcon';
 export * from './RainIcon/RainIcon';
+export * from './SnowIcon/SnowIcon';
