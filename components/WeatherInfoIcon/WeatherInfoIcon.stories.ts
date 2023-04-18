088b3d4f2a640c88c62243578dabe3bf5e26447b
Trentin Gillis <trentin.gillis@gmail.com>
2023-04-17 20:00:27 -0600

    Update story for weather info icon component

--- components/WeatherInfoIcon/WeatherInfoIcon.stories.ts
+++ components/WeatherInfoIcon/WeatherInfoIcon.stories.ts
@@ -3,14 +3,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { WeatherInfoIcon as WeatherInfoIconComponent } from './WeatherInfoIcon';
 
 const meta: Meta<typeof WeatherInfoIconComponent> = {
-  title: 'Weather Icons',
+  title: 'Weather Info Icons',
   component: WeatherInfoIconComponent,
 };
 
 export default meta;
 type Story = StoryObj<typeof WeatherInfoIconComponent>;
 
-export const WeatherIcon: Story = {
+export const WeatherInfoIcon: Story = {
   args: {
     icon: 'humidity',
     size: 160,
