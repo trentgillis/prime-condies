ac5cac37d2b4f09e4b5e709603733740a63fe199
Trentin Gillis <trentin.gillis@gmail.com>
2023-04-23 15:48:30 -0600

    Update imports

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export * from './Nav/Nav';
 export * from './Logo/Logo';
 export * from './SearchBar/SearchBar';
 export * from './Typography';
+export * from './WeatherIcon/WeatherIcon';
+export * from './WeatherInfoIcon/WeatherInfoIcon';
