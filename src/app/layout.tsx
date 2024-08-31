5a0cbfc3acea0065f0904dc920681e0cdc7cc442
Trentin Gillis <trentin.gillis@gmail.com>
2024-08-31 13:50:39 -0600

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,16 @@ import "@/styles/global.scss";
 
 import type { Metadata } from "next";
 
+import { Outfit, Open_Sans } from "next/font/google";
+
+const outfit = Outfit({
+  variable: "--font-outfit",
+});
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+});
+
 export const metadata: Metadata = {
   title: "PrimeCondies",
   description: "A weather app for rock climbers",
@@ -15,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${outfit.variable} ${openSans.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
