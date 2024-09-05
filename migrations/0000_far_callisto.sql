3c48dead3b7a927253d02e79cab8145a07924fab
Trentin Gillis <trentin.gillis@gmail.com>
2024-09-04 19:21:24 -0600

    setup database connection and drizzle

---
+++ migrations/0000_far_callisto.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS "areas_table" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(256) NOT NULL
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import "server-only";
+
+import { db } from "@/db";
 import styles from "./page.module.scss";
 
 import React from "react";
+import { AreaTable } from "@/db/schema";
 
-export default function Home() {
-  return <main className={styles.main}>PrimeCondies</main>;
+export default async function Home() {
+  const areas = await db.select().from(AreaTable);
+
+  return (
+    <main className={styles.main}>
+      {areas.map((area) => (
+        <p>{area.name}</p>
+      ))}
+    </main>
+  );
 }
