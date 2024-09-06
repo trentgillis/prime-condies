299be2b35a83f10eb73e5f2572791ab2cc4b8c2a
Trentin Gillis <trentin.gillis@gmail.com>
2024-09-05 22:51:52 -0600

    implement title component for headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import "server-only";
+import 'server-only';
 
-import { db } from "@/db";
-import styles from "./page.module.scss";
+import { db } from '@/db';
+import styles from './page.module.scss';
 
-import React from "react";
-import { AreaTable } from "@/db/schema";
+import React from 'react';
+import { AreaTable } from '@/db/schema';
+import Title from '@/components/Title';
 
 export default async function Home() {
   const areas = await db.select().from(AreaTable);
 
   return (
     <main className={styles.main}>
+      <Title variant="h1">Areas</Title>
       {areas.map((area) => (
         <p key={area.id}>{area.name}</p>
       ))}
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,48 @@
+import styles from './Title.module.scss';
+
+import React, { PropsWithChildren } from 'react';
+
+function H1({ children }: PropsWithChildren) {
+  return <h1 className={`${styles.title} ${styles.h1}`}>{children}</h1>;
+}
+
+function H2({ children }: PropsWithChildren) {
+  return <h2 className={`${styles.title} ${styles.h2}`}>{children}</h2>;
+}
+
+function H3({ children }: PropsWithChildren) {
+  return <h3 className={`${styles.title} ${styles.h3}`}>{children}</h3>;
+}
+
+function H4({ children }: PropsWithChildren) {
+  return <h4 className={`${styles.title} ${styles.h4}`}>{children}</h4>;
+}
+
+function H5({ children }: PropsWithChildren) {
+  return <h5 className={`${styles.title} ${styles.h5}`}>{children}</h5>;
+}
+
+function H6({ children }: PropsWithChildren) {
+  return <h6 className={`${styles.title} ${styles.h6}`}>{children}</h6>;
+}
+
+const titleComponents = {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
+};
+
+interface TitleProps extends PropsWithChildren {
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+}
+
+function Title({ variant, children }: TitleProps) {
+  const TitleComponent = titleComponents[variant] ?? H1;
+
+  return <TitleComponent>{children}</TitleComponent>;
+}
+
+export default Title;
