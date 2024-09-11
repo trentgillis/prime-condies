2b07fb030e2c511822d1d56b9282c2455f2a2ecf
Trentin Gillis <trentin.gillis@gmail.com>
2024-09-10 22:35:16 -0600

    add a few more areas to the seed file for testing

--- src/db/areas.ts
+++ src/db/areas.ts
@@ -3,10 +3,25 @@ import { AreaTable } from './schema';
 
 const areas: InferInsertModel<typeof AreaTable>[] = [
   {
+    id: 1,
     name: 'Eleven Mile Canyon',
     place: 'Colorado',
     countryCode: 'USA',
     location: { x: -105.39031100011304, y: 38.952944973361745 },
+  },
+  {
+    id: 2,
+    name: 'Boulder Canyon',
+    place: 'Colorado',
+    countryCode: 'USA',
+    location: { x: -105.45476358906625, y: 39.97983493878136 },
+  },
+  {
+    id: 3,
+    name: 'Newlin Creek',
+    place: 'Colorado',
+    countryCode: 'USA',
+    location: { x: -105.19522954244155, y: 38.25898012121234 },
   },
 ];
 
