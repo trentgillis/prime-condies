c7fef16c487b1bf4e2b6faa7de6b092c31d9805a
Trentin Gillis <trentin.gillis@gmail.com>
2024-09-14 10:18:07 -0600

    update db setup script to do full seed of areas table

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -6,24 +6,31 @@ import areas from './areas';
 loadEnvConfig(process.cwd());
 
 async function setupAreas() {
-  await sql.query('DROP TABLE IF EXISTS areas_table');
-  return sql.query(`
-    CREATE TABLE IF NOT EXISTS areas_table (
+  console.log('🔄 Setting up areas table...');
+  await sql.query('DROP TABLE IF EXISTS areas');
+  await sql.query(`
+    CREATE TABLE IF NOT EXISTS areas (
       id serial PRIMARY KEY NOT NULL,
       name varchar(256) NOT NULL,
       place varchar(256) NOT NULL,
       country_code varchar(3) NOT NULL
     );
 `);
+  console.log('✅ Areas table successfully created');
 }
 
 async function seedAreas() {
-  const area = areas[0];
-  await sql.query(`
-      INSERT INTO areas_table (name, place, country_code)
+  console.log('🌱 Seeding area data...');
+  await Promise.all(
+    areas.map((area) => {
+      return sql.query(`
+      INSERT INTO areas (name, place, country_code)
       VALUES ('${area.name}', '${area.place}', '${area.countryCode}')
       ON CONFLICT (id) DO NOTHING;
     `);
+    }),
+  );
+  console.log('✅ Areas successfully seeded');
 }
 
 (async function setupDb() {
