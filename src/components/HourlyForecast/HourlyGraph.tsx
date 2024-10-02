f2c487bf692cbf83df7c417202db8d919f3b5448
Trentin Gillis <trentin.gillis@gmail.com>
2024-10-01 20:13:10 -0600

    get a chart rendering on the details page for hourly data

--- src/components/HourlyForecast/HourlyGraph.tsx
+++ src/components/HourlyForecast/HourlyGraph.tsx
@@ -1,11 +1,27 @@
+'use client';
+
 import styles from './HourlyGraph.module.scss';
 
 import React from 'react';
+import { Area, AreaChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
-function HourlyGraph() {
+import { HourlyWeatherData } from '@/lib/types/WeatherResponse';
+
+interface HourlyGraphProps {
+  hourlyForecast: HourlyWeatherData[];
+}
+
+function HourlyGraph({ hourlyForecast }: HourlyGraphProps) {
   return (
     <div className={styles.wrapper}>
       <h1>HourlyGraph</h1>
+      <ResponsiveContainer width="100%" height={380}>
+        <AreaChart data={hourlyForecast}>
+          <XAxis dataKey="dt" />
+          <YAxis tick={{ fill: '#fff', height: 2, fontSize: 12 }} />
+          <Area dataKey="temp" />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 }
