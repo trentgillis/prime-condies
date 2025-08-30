7683bc8f5ffb300c43e8a358f0b989c356f4afc7
Trentin Gillis <trentin.gillis@gmail.com>
2025-08-30 15:49:26 -0600

    add prettier plugin to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,23 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends('next', 'next/core-web-vitals', 'next/typescript'),
-},  {
-  ignores: [
-    "next-env.d.ts",
-    "**/node_modules/**",
-    "**/.next/**",
-    "**/out/**",
-    "**/build/**",
-    "**/dist/**",
-  ],
-}]);
-
-
+export default defineConfig([
+  {
+    extends: compat.extends('next', 'next/core-web-vitals', 'next/typescript', 'prettier'),
+  },
+  {
+    ignores: ['next-env.d.ts', '**/node_modules/**', '**/.next/**', '**/out/**', '**/build/**', '**/dist/**'],
+  },
+]);
